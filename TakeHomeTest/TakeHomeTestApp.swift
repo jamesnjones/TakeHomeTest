10e29786c9fed847001cbe05f380399f59322808
James <james>
2022-07-15 18:46:03 +0100

    Initial Commit

---
+++ TakeHomeTest/TakeHomeTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TakeHomeTestApp.swift
+//  TakeHomeTest
+//
+//  Created by james Jones on 15/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct TakeHomeTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
